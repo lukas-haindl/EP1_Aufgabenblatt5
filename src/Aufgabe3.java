344e489f365c6350f2f0b67d14f7b0395416cc47
lukas-haindl <lukas.haindl03@icloud.com>
2024-12-08 19:18:12 +0100

    complete Aufgabe3;

--- src/Aufgabe3.java
+++ src/Aufgabe3.java
@@ -9,14 +9,31 @@ public class Aufgabe3 {
     private static boolean[][] genAnimalCompound(int compoundSize, float probability) {
 
         Random myRand = new Random(5); // Diese Zeile nicht verändern!
-
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return null; //Zeile kann geändert oder entfernt werden.
+        boolean[][] array = new boolean[compoundSize][compoundSize];
+        for (int i = 0; i < compoundSize; i++) {
+            for (int j = 0; j < compoundSize; j++) {
+                array[i][j] = myRand.nextFloat() < probability;
+            }
+        }
+        return array; //Zeile kann geändert oder entfernt werden.
     }
 
     private static int[][] calcAnimalDensity(boolean[][] animalCompound) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Methode
-        return null; //Zeile kann geändert oder entfernt werden.
+        int[][] array = new int[animalCompound.length][animalCompound[0].length];
+        for (int i = 0; i < animalCompound.length; i++) {
+            for (int j = 0; j < animalCompound[0].length; j++) {
+                int neighbours = 0;
+                for (int k = i - 1; k <= i + 1 ; k++) {
+                    for (int l = j - 1; l <= j + 1 ; l++) {
+                        if ((!(l < 0 || l >= animalCompound.length)) && (!(k < 0 || k >= animalCompound.length))) {
+                            neighbours += animalCompound[k][l] ? 1 : 0;
+                        }
+                    }
+                }
+                array[i][j] = neighbours;
+            }
+        }
+        return array; //Zeile kann geändert oder entfernt werden.
     }
 
     // helping method for printing the animal compound
@@ -48,11 +65,29 @@ public class Aufgabe3 {
 
         // variables
         int compoundSize = 8;
-        float probability = 0.8f;
+        float probability = 0.2f;
+        System.out.println("--compoundSize= " + compoundSize + ", probability= " + probability + " ----");
         boolean[][] animalCompound = genAnimalCompound(compoundSize, probability);
         printCompound(animalCompound);
         System.out.println();
         int[][] animalDensity = calcAnimalDensity(animalCompound);
         printDensity(animalDensity);
+
+        System.out.println("--compoundSize= " + compoundSize + ", probability= " + probability + " ----");
+        probability = 0.5f;
+        animalCompound = genAnimalCompound(compoundSize, probability);
+        printCompound(animalCompound);
+        System.out.println();
+        animalDensity = calcAnimalDensity(animalCompound);
+        printDensity(animalDensity);
+
+        System.out.println("--compoundSize= " + compoundSize + ", probability= " + probability + " ----");
+        probability = 0.8f;
+        animalCompound = genAnimalCompound(compoundSize, probability);
+        printCompound(animalCompound);
+        System.out.println();
+        animalDensity = calcAnimalDensity(animalCompound);
+        printDensity(animalDensity);
+
     }
 }
